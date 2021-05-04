6f3dbe72a46301729c681aea75c440e95be11f5f
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-05-04 11:43:52 +0100

    Migrate to GH actions

--- spec/lib/pbio/delimited_spec.rb
+++ spec/lib/pbio/delimited_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe PBIO::Delimited do
+  subject { described_class.new io }
+
   let(:io)   { StringIO.new }
   let(:msg1) { PBIO::Spec::Message.new(name: 'Test A') }
   let(:msg2) { PBIO::Spec::Message.new(name: 'Test B') }
   let(:msg3) { PBIO::Spec::Message.new(name: 'x' * 32_000) }
 
-  subject { described_class.new io }
-
-  it 'should write' do
+  it 'writes' do
     expect(subject.write(msg1)).to eq(9)
     expect(subject.write(msg2)).to eq(9)
     expect(io.size).to eq(18)
@@ -18,13 +18,13 @@ describe PBIO::Delimited do
     expect(io.size).to eq(32_025)
   end
 
-  it 'should read' do
+  it 'reads' do
     expect(subject.write(msg1)).to eq(9)
     expect(subject.write(msg2)).to eq(9)
     io.rewind
 
     expect(subject.read(PBIO::Spec::Message)).to eq(msg1)
-    expect(subject).to_not be_eof
+    expect(subject).not_to be_eof
     expect(subject.read(PBIO::Spec::Message)).to eq(msg2)
     expect(subject).to be_eof
     expect(subject.read(PBIO::Spec::Message)).to be_nil
