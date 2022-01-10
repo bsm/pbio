990784057bab5d5d0afb075a5c085a55950cc1f6
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2022-01-10 09:39:12 +0000

    Bump deps

--- pbio_test.go
+++ pbio_test.go
@@ -2,11 +2,13 @@ package pbio_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"math"
 	"testing"
 
 	"github.com/bsm/pbio"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -23,60 +25,70 @@ var message, _ = structpb.NewValue(map[string]interface{}{
 	},
 })
 
-var _ = Describe("Encoder", func() {
-	var subject *pbio.Encoder
-	var buf *bytes.Buffer
+func TestEncoder(t *testing.T) {
+	buf := new(bytes.Buffer)
+	enc := pbio.NewEncoder(buf)
 
-	BeforeEach(func() {
-		buf = new(bytes.Buffer)
-		subject = pbio.NewEncoder(buf)
-	})
+	if err := enc.Encode(message); err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	}
+	if exp, got, delta := 192, buf.Len(), 20; math.Abs(float64(exp)-float64(got)) > float64(delta) {
+		t.Fatalf("expected %d (±%d), got %d", exp, delta, got)
+	}
 
-	It("should encode", func() {
-		Expect(subject.Encode(message)).To(Succeed())
-		Expect(buf.Len()).To(BeNumerically("~", 192, 20))
-		Expect(subject.Encode(message)).To(Succeed())
-		Expect(buf.Len()).To(BeNumerically("~", 384, 20))
-	})
-})
+	if err := enc.Encode(message); err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	}
+	if exp, got, delta := 384, buf.Len(), 20; math.Abs(float64(exp)-float64(got)) > float64(delta) {
+		t.Fatalf("expected %d (±%d), got %d", exp, delta, got)
+	}
+}
 
-var _ = Describe("Decoder", func() {
-	var subject *pbio.Decoder
+func TestDecoder(t *testing.T) {
+	buf := new(bytes.Buffer)
+	enc := pbio.NewEncoder(buf)
+	for i := 0; i < 2; i++ {
+		if err := enc.Encode(message); err != nil {
+			t.Fatalf("expected no error, but got %v", err)
+		}
+	}
 
-	BeforeEach(func() {
-		buf := new(bytes.Buffer)
-		enc := pbio.NewEncoder(buf)
-		Expect(enc.Encode(message)).To(Succeed())
-		Expect(enc.Encode(message)).To(Succeed())
+	dec := pbio.NewDecoder(buf)
+	m1 := new(structpb.Value)
+	if err := dec.Decode(m1); err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	}
 
-		subject = pbio.NewDecoder(buf)
-	})
+	if data, err := m1.MarshalJSON(); err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	} else if exp, got := `{
+  "address": {
+    "city": "New York",
+    "postalCode": "10021-3100",
+    "state": "NY",
+    "streetAddress": "21 2nd Street"
+  },
+  "age": 27,
+  "firstName": "John",
+  "isAlive": true,
+  "lastName": "Smith"
+}`, normJSON(data); exp != got {
+		t.Fatalf("expected:\n%v, but got:\n%v", exp, got)
+	}
 
-	It("should decode", func() {
-		m1 := new(structpb.Value)
-		Expect(subject.Decode(m1)).To(Succeed())
-		Expect(m1.MarshalJSON()).To(MatchJSON(`{
-			"firstName":"John",
-			"lastName":"Smith",
-			"isAlive":true,
-			"age":27,
-			"address":{
-				"city":"New York",
-				"postalCode":"10021-3100",
-				"state":"NY",
-				"streetAddress":"21 2nd Street"
-			}
-		}`))
+	m2 := new(structpb.Value)
+	if err := dec.Decode(m2); err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	}
 
-		m2 := new(structpb.Value)
-		Expect(subject.Decode(m2)).To(Succeed())
+	m3 := new(structpb.Value)
+	if exp, got := io.EOF, dec.Decode(m3); !errors.Is(got, exp) {
+		t.Fatalf("expected %v, but got %v", exp, got)
+	}
+}
 
-		m3 := new(structpb.Value)
-		Expect(subject.Decode(m3)).To(MatchError(`EOF`))
-	})
-})
-
-func TestSuite(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "pbio")
+func normJSON(data []byte) string {
+	b := new(bytes.Buffer)
+	_ = json.Indent(b, data, "", "  ")
+	return b.String()
 }
